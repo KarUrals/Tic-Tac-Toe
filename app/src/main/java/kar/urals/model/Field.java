728a0f94161e4c1ebef0905240cc51dd94ad220e
Karelskiy_Andrey <karelskiy.a@gmail.com>
2022-11-17 16:01:30 +0200

    Field checkPoint() covered by tests

--- app/src/main/java/kar/urals/model/Field.java
+++ app/src/main/java/kar/urals/model/Field.java
@@ -1,20 +1,45 @@
 package kar.urals.model;
 
+import kar.urals.exceptions.AlreadyOccupiedException;
+import kar.urals.exceptions.InvalidPointException;
+
 import java.awt.Point;
 
 public class Field {
     private static final int FIELD_SIZE = 3;
+    public static final int MIN_COORDINATE = 0;
+    public static final int MAX_COORDINATE = FIELD_SIZE;
     private final Figure[][] field = new Figure[FIELD_SIZE][FIELD_SIZE];
 
     public int getSize() {
         return FIELD_SIZE;
     }
 
-    public Figure getFigure(final Point point) {
+    public Figure getFigure(final Point point) throws InvalidPointException {
+        if (!checkPoint(point)) {
+            throw new InvalidPointException();
+        }
+
         return field[point.x][point.y];
     }
 
-    public void setFigure(final Point point, final Figure figure) {
+    public void setFigure(final Point point, final Figure figure) throws InvalidPointException, AlreadyOccupiedException {
+        if (!checkPoint(point)) {
+            throw new InvalidPointException();
+        }
+
+        if (field[point.x][point.y] != null) {
+            throw new AlreadyOccupiedException();
+        }
+
         field[point.x][point.y] = figure;
+    }
+
+    private boolean checkPoint(Point point) {
+        return checkCoordinate(point.x) && checkCoordinate(point.y);
+    }
+
+    private boolean checkCoordinate(final int coordinate) {
+        return coordinate >= MIN_COORDINATE && coordinate < MAX_COORDINATE;
     }
 }
--- app/src/test/java/kar/urals/model/FieldTest.java
+++ app/src/test/java/kar/urals/model/FieldTest.java
@@ -1,5 +1,7 @@
 package kar.urals.model;
 
+import kar.urals.exceptions.InvalidPointException;
+import kar.urals.exceptions.XOException;
 import org.junit.jupiter.api.Test;
 import java.awt.Point;
 
@@ -15,7 +17,7 @@ class FieldTest {
     }
 
     @Test
-    void testSetFigure() {
+    void testSetFigure() throws XOException {
         final Field field = new Field();
         final Point inputPoint = new Point(0, 0);
         final Figure inputFigure = Figure.X;
@@ -25,5 +27,59 @@ class FieldTest {
         final Figure actualFigure = field.getFigure(inputPoint);
 
         assertEquals(expectedFigure, actualFigure);
+    }
+
+    @Test
+    void testGetFigureWhenFigureIsNotSet() throws XOException {
+        final Field field = new Field();
+        final Point inputPoint = new Point(0, 0);
+
+        final Figure actualFigure = field.getFigure(inputPoint);
+
+        assertNull(actualFigure);
+    }
+
+    @Test
+    void testGetFigureWhenXIsLessThenZero() throws XOException {
+        final Field field = new Field();
+        final Point inputPoint = new Point(-1, 0);
+
+        try {
+            field.getFigure(inputPoint);
+            fail();
+        } catch (final InvalidPointException e) {}
+    }
+
+    @Test
+    void testGetFigureWhenYIsLessThenZero() throws XOException {
+        final Field field = new Field();
+        final Point inputPoint = new Point(0, -1);
+
+        try {
+            field.getFigure(inputPoint);
+            fail();
+        } catch (final InvalidPointException e) {}
+    }
+
+    @Test
+    void testGetFigureWhenXIsMoreThenSize() throws XOException {
+        final Field field = new Field();
+        final Point inputPoint = new Point(field.getSize() + 1, 0);
+
+        try {
+            field.getFigure(inputPoint);
+            fail();
+        } catch (final InvalidPointException e) {}
+    }
+
+    @Test
+    void testGetFigureWhenYIsMoreThenSize() throws XOException {
+        final Field field = new Field();
+        final Point inputPoint = new Point(0, field.getSize() + 1);
+
+        try {
+            field.getFigure(inputPoint);
+            fail();
+        } catch (final InvalidPointException e) {}
     }
 }
