699666b0fa39d5e770926bbce280518a9070f6e4
Karelskiy_Andrey <karelskiy.a@gmail.com>
2022-11-18 15:15:28 +0200

    MoveController created. Field and FieldTest updated

---
+++ app/src/main/java/kar/urals/controllers/MoveController.java
@@ -0,0 +1,21 @@
+package kar.urals.controllers;
+
+import kar.urals.exceptions.AlreadyOccupiedException;
+import kar.urals.exceptions.InvalidPointException;
+import kar.urals.model.Field;
+import kar.urals.model.Figure;
+
+import java.awt.Point;
+
+public class MoveController {
+
+    public void applyField(final Field field,
+                           final Point point,
+                           final Figure figure) throws InvalidPointException, AlreadyOccupiedException {
+        if (field.getFigure(point) != null) {
+            throw new AlreadyOccupiedException();
+        }
+
+        field.setFigure(point, figure);
+    }
+}
